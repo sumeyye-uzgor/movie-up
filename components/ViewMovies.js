1aad0b8f97540dce9e746b1df470ba5d56c414fa
Sümeyye Üzgör <summeyyeoz@gmail.com>
2021-04-12 16:01:08 +0300

    provide unique key error is fixed

--- components/ViewMovies.js
+++ components/ViewMovies.js
@@ -6,7 +6,7 @@ function ViewMovies({ movies }) {
         <div className={styles.moviesContainer}>
             {
                 movies.length > 0 ?
-                    movies.map(movie => <ViewMovieContainer movie={movie} key={movie.imdbId} />)
+                    movies.map(movie => <ViewMovieContainer movie={movie} key={movie.imdbID} />)
                     :
                     <div className={styles.titleContainer}>
                         The movie that you search couldn't be found. Please try again.
